63005f889d0d9a92e14c75a76097be4581480895
liubiwei <liubiwei19990228@163.com>
2025-02-27 16:55:42 +0800

    fix

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 import { contextArr } from "../constant/text.js";
 import { getRandomColor, } from "../utils/a.js";
 
-let view = document.getElementById("view");
 let mask = document.getElementById("mask");
 let fluid_area = document.querySelectorAll(".fluid-area");
+let fluidAreaBlockParams
 window.onload = () => {
-    contextArr.forEach((item, index) => {
+    contextArr.forEach(function (item, index) {
         const divEle = document.createElement('div');
         divEle.className = 'fluid-area-block' + index;
         divEle.textContent = item;
@@ -15,27 +15,38 @@ window.onload = () => {
 
         let fluidAreaBlock = document.querySelector('.fluid-area-block' + index);
         function onclickfluidAreaBlock() {
+            fluidAreaBlock.style.display = "block";
+            fluidAreaBlock.style.height = "60vw";
+            fluidAreaBlock.style.width = "60vw";
             fluidAreaBlock.style.overflowY = "auto";
             fluidAreaBlock.style.position = "absolute";
             fluidAreaBlock.style.top = "50%";
             fluidAreaBlock.style.left = "50%";
-            fluidAreaBlock.style.transform = "translate(-50%,-50%) scale(2,2)";
+            fluidAreaBlock.style.transform = "translate(-50%,-50%)";
             fluidAreaBlock.style.zIndex = "1002";
+            fluidAreaBlock.style.color = "#000";
             mask.style.display = "block";
-            view.style.overflowY = "hidden";
+            fluidAreaBlockParams=fluidAreaBlock
         }
         fluidAreaBlock.addEventListener("click", onclickfluidAreaBlock);
-        function maskOnclick() {
-            mask.style.display = "none";
-            fluidAreaBlock.style.overflowY = "hidden";
-            fluidAreaBlock.style.position = "static";
-            fluidAreaBlock.style.transform = "";
-            fluidAreaBlock.style.zIndex = "auto";
-            view.style.overflowY = "auto";
-        }
-        mask.addEventListener("click", maskOnclick);
     })
 };
+function maskOnclick(fluidAreaBlock) {
+    if(fluidAreaBlock){
+        mask.style.display = "none";
+        fluidAreaBlock.style.display = "-webkit-box";
+        fluidAreaBlock.style.height = "auto";
+        fluidAreaBlock.style.width = "auto";
+        fluidAreaBlock.style.overflowY = "hidden";
+        fluidAreaBlock.style.position = "static";
+        fluidAreaBlock.style.transform = "scale(1)";
+        fluidAreaBlock.style.color =getRandomColor();
+        fluidAreaBlock.style.zIndex = "auto";
+    }
+}
+mask.addEventListener("click",function(){
+    maskOnclick(fluidAreaBlockParams)
+} );
 
 let hospitalEle = document.querySelector(".hospital");
 let isMove = false;  // 标记是否正在拖动
